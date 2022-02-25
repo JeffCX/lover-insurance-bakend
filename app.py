e4457bd031025c0df32f61cc8d93969868b281bc
ZiWei Qiu <ziweiqiu@ZiWeis-MacBook-Pro.local>
2022-02-25 12:06:52 -0500

    commit

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, request
-import json
-from flask_cors import CORS
+from flask import Flask
+# import json
+from flask import  request
 
+from flask_cors import CORS
+# import jsonify
 app = Flask(__name__)
 CORS(app)
+
+q_list = ["1" ,"2" ,"3"]
+rows, cols = (len(q_list), 2)
+qa_list = [[0 for i in range(cols)] for j in range(rows)]
+for i in range(len(q_list)):
+    qa_list[i][0] = q_list[i]
+    # qa_list[i][1]=[]
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -19,11 +28,29 @@ def buy_insurance():
 
 @app.route('/get-question', methods=['GET'])
 def get_question():
-    return "hi"
+    return {
+        "result": q_list
+    }
 
 @app.route('/update-question', methods=['GET'])
 def update_question():
-    return "hi"
+    idx=request.args["idx"]
+    idx=int(idx)
+    return {
+        "result": q_list[idx]
+    }
+
+@app.route('/get_answer', methods=['GET'])
+def get_answer():
+    idx=request.args["idx"]
+    ans=request.args["ans"]
+    idx=int(idx)
+    qa_list[idx][1] = ans
+    return{
+
+        "result": qa_list
+    }
+
 
 @app.route('/claim-middle-reward', methods=['GET'])
 def claim_middle_reward():
